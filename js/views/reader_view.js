5d65d48aadc41a8c303c8306bf81230def7a9ea5
mohit_019 <mohit.aggarwal@students.iiit.ac.in>
2015-08-14 17:17:53 +0530

    Changes made to js/views/reader_view.js file.

--- js/views/reader_view.js
+++ js/views/reader_view.js
@@ -605,7 +605,7 @@ var ReaderView = function (options) {
         var currentSpineItem = _spine.getItemById(lastOpenPage.idref);
 
         // If the current SpineItem is non-linear then go to Bookmarked position when you press Previous button.
-        if (!_spine.isValidLinearItem(currentSpineItem)) {
+        if (currentSpineItem.linear == 'no') {
             self.openSpineItemElementCfi(globalBookmark.idref, globalBookmark.contentCFI, self);
             return;
         }
@@ -648,7 +648,7 @@ var ReaderView = function (options) {
         var currentSpineItem = _spine.getItemById(firstOpenPage.idref);
         
         // If the current SpineItem is non-linear then go to Bookmarked position when you press Previous button.
-        if (!_spine.isValidLinearItem(currentSpineItem)) {
+        if (currentSpineItem.linear == 'no') {
             self.openSpineItemElementCfi(globalBookmark.idref, globalBookmark.contentCFI, self);
             return;
         }
@@ -674,7 +674,7 @@ var ReaderView = function (options) {
         var currentSpineItem = _spine.getItemById(firstOpenPage.idref);
 
         // If the current SpineItem is non-linear then go to Bookmarked position when you press Previous button.
-        if( !_spine.isValidLinearItem(currentSpineItem) ){
+        if (currentSpineItem.linear == 'no') {
             self.openSpineItemElementCfi(globalBookmark.idref, globalBookmark.contentCFI, self);
             return;
         }
